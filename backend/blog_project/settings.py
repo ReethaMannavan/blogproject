23ad05f0cc27dc32ed9dfdf7c2bbf5af76d56430
ReethaMannavan <108715512+ReethaMannavan@users.noreply.github.com>
2025-09-19 09:47:22 +0300

    Update settings.py

--- backend/blog_project/settings.py
+++ backend/blog_project/settings.py
@@ -137,7 +137,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:5173",
-  
+    "https://famous-biscochitos-675d7d.netlify.app"
 
 ]
 
@@ -192,4 +192,5 @@ EMAIL_HOST_PASSWORD = 'fris hfvj tbok ixrl'  # Gmail App Password
 
 
 # Default "from" email
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
